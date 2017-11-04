6dbdc81f6fa6913904fb135da889f5ecde9433f6
JovanR <JovanR@vt.edu>
2017-11-03 22:33:55 -0400

    transfering recent to pi

--- bridge.py
+++ bridge.py
@@ -1,2 +1,17 @@
-# bridge.py 
-import rmq_params.py
+# bridge.py
+import pymongo
+import pika
+import sys
+from pymongo import MongoClient
+from rmq_params.py import *
+from bluetooth import *
+
+
+
+# Create mongo client with default host and port
+# client = MongoClient()
+# Connect host and port
+# client = MongoClient('localhost', 27017)
+# Access database
+
+# rabbitMQHostName = sys.argv[2]
--- repository.py
+++ repository.py
@@ -1,2 +1,11 @@
 #repository.py
 import rmq_params.py
+import sys
+import pika
+
+# 10 for GPIO.BOARD or 11 for GPIO.BCM
+gpioMode = sys.argv[2]
+redPin = sys.argv[4]
+greenPin = sys.argv[6]
+bluePin = sys.argv[8]
+

65aa254de616db578444112bb57e2ae6dbe84854
JovanR <JovanR@vt.edu>
2017-11-05 09:35:03 -0500

    bluetooth send test

--- bridge.py
+++ bridge.py
@@ -9,14 +9,15 @@ from bluetooth import *
 
 # rabbitMQHostName = sys.argv[2]
 
-server_sock = BluetoothSocket(RFCOMM)
+# Bluetooth initialization
+server_sock = bluetooth.BluetoothSocket(RFCOMM)
 server_sock.bind(("", PORT_ANY))
 server_sock.listen(1)
 
 port = server_sock.getsockname()[1]
 
 uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
-
+# wait for a connection
 advertise_service(server_sock, "SampleServer",
                   service_id=uuid,
                   service_classes=[uuid, SERIAL_PORT_CLASS],
@@ -25,33 +26,40 @@ advertise_service(server_sock, "SampleServer",
                   )
 
 print("Waiting for connection on RFCOMM channel %d" % port)
-
+# Connect with device
 client_sock, client_info = server_sock.accept()
 print("Accepted connection from ", client_info)
 
 # MongoDB Initializations: Connect host and port
-# client = MongoClient('localhost', 27017)
-
-
+client = MongoClient('localhost', 27017)
 
 # Access database
-db = client['test-database']  # Same as warehouse?
+dbName = 'test-database'
+db = client[dbName]  # Same as warehouse?
 collection = db['test-collection']
 posts = db.posts
+
+# Speak back to bluetooth
+client_sock.send("Available queues")
+for queue in rmq_params:
+    client_sock.send(queue)
 
 try:
     while True:
         data = client_sock.recv(1024)
-        data = str(data).split("'")
+        data = str(data).split("':")
+        print(data)
         data = data[1]
         print(data[0])
         
         if data[0] == 'p':
+
             print('this is for produce')
+            """""
             channel.basic_publish(exchange='Squires',
                                   routing_key='wishes',
                                   body='I wish I remembered their name')
-            
+            """
         elif data[0] == 'c':
             print('this is for consume')
         
